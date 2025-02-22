507d53cbd47d9b7f79985b13dab6614e36c127ee
Maha Maryam <119899393+mahamaryam@users.noreply.github.com>
2025-02-22 12:12:04 +0500

    Update execve.s

--- execve.s
+++ execve.s
@@ -1,19 +1,19 @@
 
 .global _start
 _start:
-    mov r7, #11             @ Syscall: execve
-    ldr r0, =script_path    @ Address of the script path (filename)
-    ldr r1, =argv           @ Address of arguments array
-    mov r2, #0              @ No environment variables (envp = NULL)
-    swi 0                   @ Call the kernel
+    mov r7, #11             
+    ldr r0, =script_path   
+    ldr r1, =argv          
+    mov r2, #0             
+    swi 0                  
 
-    mov r7, #1              @ Syscall: exit
-    mov r0, #0              @ Exit code 0
-    swi 0                   @ Exit program
+    mov r7, #1             
+    mov r0, #0             
+    swi 0                  
 
 .data
-script_path: .asciz "./script.sh"    @ Path to your shell script
-argv:       .word script_path, 0           @ Arguments array (NULL-terminated)
+script_path: .asciz "./script.sh"   
+argv:       .word script_path, 0          
 
 
 
