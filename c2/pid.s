86c07c04a30acb60a13996881dce5c91f13fbc69
Maha Maryam <119899393+mahamaryam@users.noreply.github.com>
2025-02-22 12:17:16 +0500

    Update pid.s

--- c2/pid.s
+++ c2/pid.s
@@ -1,28 +1,24 @@
 .global _asm_start
-.extern sprintf       // Declare the external sprintf function
-.extern printf        // Declare the external printf function
+.extern sprintf       
+.extern printf        
 
 .data
-    result_fmt: .asciz "PID: %d\n"  // Format string for sprintf
-    output: .space 50                          // Buffer for output
+    result_fmt: .asciz "PID: %d\n"  
+    output: .space 50               
 
 .text
 _asm_start:
-    // Pass arguments to the add function
     mov r7,#0x14
     swi 0
-    // Use sprintf to format the result into a string
-    ldr r1, =result_fmt  // Format string
-    ldr r2, =output      // Buffer to store the result
-    mov r3, r0          // Move the result into r3 (argument to sprintf)
-    bl sprintf           // Call sprintf
+    ldr r1, =result_fmt 
+    ldr r2, =output     
+    mov r3, r0          
+    bl sprintf          
 
-    // Print the result using printf
-    ldr r0, =output     // Address of the output buffer
-    bl printf           // Call printf to print the formatted string
+    ldr r0, =output    
+    bl printf           
 
-    // Exit the program
-    mov r0, #0          // Exit status 0
-    mov r7, #1          // Syscall number for sys_exit
-    swi 0               // Make the syscall to exit
+    mov r0, #0         
+    mov r7, #1          
+    swi 0               
 
